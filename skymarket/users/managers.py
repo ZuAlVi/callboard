2d405ebae6c8c5c2aa5b12ac1aac8970c4b3a0c2
ZuAlVi <134595256+ZuAlVi@users.noreply.github.com>
2024-04-06 10:46:50 +0300

    Update managers.py

--- skymarket/users/managers.py
+++ skymarket/users/managers.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     """ Класс создания пользователя"""
 
-    def create_user(self, email, first_name, password, last_name=None, phone=None):
+    def create_user(self, email, first_name, password, last_name=None, phone=None, image=None):
         """Функция создания пользователя — в нее мы передаем обязательные поля"""
         if not email:
             raise ValueError('Users must have an email address')
@@ -17,7 +17,8 @@ class UserManager(BaseUserManager):
             first_name=first_name,
             last_name=last_name,
             phone=phone,
-            role='user'
+            role='user',
+            image=image
         )
         user.is_active = True
         user.set_password(password)
@@ -25,7 +26,7 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self, email, first_name, password, last_name=None, phone=None):
+    def create_superuser(self, email, first_name, password, last_name=None, phone=None, image=None):
         """
         функция для создания суперпользователя — с ее помощью мы создаем админинстратора
         это можно сделать с помощью команды createsuperuser
@@ -37,7 +38,8 @@ class UserManager(BaseUserManager):
             last_name=last_name,
             phone=phone,
             password=password,
-            role='admin'
+            role='admin',
+            image=image
         )
 
         user.is_active = True
